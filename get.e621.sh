dcb809dba34162ef0971532029ebfdd74962a894
Pavel Frolov <tigrashik@gmail.com>
2015-08-18 12:39:30 +0300

    Приведение скриптов к общему виду. Поправил описание.

--- get.e621.sh
+++ get.e621.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
+# Юзергаент
 uag="Mozilla/5.0 (Windows; U; Windows NT 5.1; ru; rv:1.9.1.1) Gecko/20090715 Firefox/3.5.1 (.NET CLR 3.5.30729)"
 
+# Проверка параметров
 if [ ! "$2" = "" ]
 then
   tags=$1
@@ -18,21 +20,29 @@ else
   fi
 fi
 
-
-dldr='aria2c --remote-time --check-certificate=false'
+# Каталог для закачки
 if [ ! -d $savedir ]
 then
   echo Creating $savedir
   mkdir "$savedir"
-else
-  dldr='wget --no-check-certificate -nc'
 fi
 echo Entering $savedir
 cd "$savedir"
 
+# Количество постов
 postcount=`curl -# "https://e621.net/post/index.xml?tags=$tags&limit=1"|pcregrep -o 'posts\ count=\"[^"]+'|sed -e 's/posts\ count=//' -e 's/\"//'`
 echo $postcount posts
 
+# Проверка количетсва
+if [ $postcount -eq 0 ]
+then
+  echo По сочетанию "$tags" ничего не найдено.
+  exit 3
+else
+  echo По сочетанию "$tags" найдено постов: $postcount
+fi
+
+# Удаление файла-списка
 if [ -s get2.e621.txt ]
 then
   rm -f get2.e621.txt
@@ -43,7 +53,7 @@ let "pcount=postcount/100+1"
 echo $pcount
 for ((i=1; i<=$pcount; i++))
 do
-  curl "https://e621.net/post/index.xml?tags=$tags&limit=100&page=$i" -A "$uag"|pcregrep -o -e 'file_url=[^ ]+'|sed -e 's/file_url=//g' -e 's/\"//g'  >>get2.e621.txt
+  curl "https://e621.net/post/index.xml?tags=$tags&limit=100&page=$i" -A "$uag"|pcregrep -o -e 'file_url=[^ ]+'|sed -e 's/file_url=//g' -e 's/\"//g' >> get2.e621.txt
 done;
 
-$dldr -i get2.e621.txt
+wget -nc -i get2.e621.txt
