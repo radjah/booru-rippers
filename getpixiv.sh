d14e3177aa148cdd990f7be62858206a702a050e
Pavel Frolov <tigrashik@gmail.com>
2016-12-01 11:12:48 +0300

    getpixiv.sh: Полностью переделан скрипт. Авторизация и частично получение ссылок теперь идёт через API. Стало намного меньше костылей и лишних запросов.

--- getpixiv.sh
+++ getpixiv.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# Юзергаент
+# Переменные
 uag="Mozilla/5.0 (Windows NT 6.3; WOW64; rv:40.0) Gecko/20100101 Firefox/40.0"
 
 # Проверка параметров
@@ -16,21 +16,22 @@ then
   exit 1
 fi
 
+# Качалка
 dldr='aria2c --always-resume=false --max-resume-failure-tries=0 --remote-time'
 dirlet=`echo $savedir|cut -c-1`
 
+# Каталог для сохранения
 if [ ! -d ${dirlet,,}/$savedir ]
 then
   echo Creating ${dirlet,,}/$savedir
   mkdir -p "${dirlet,,}/$savedir"
 else
-  dldr='wget -nc'
+  dldr='/opt/wget/bin/wget -nc'
 fi
 echo Entering ${dirlet,,}/$savedir
 cd ${dirlet,,}/$savedir
 
-# настройки
-# id художника (athid) берется из URL вида http://www.pixiv.net/member_illust.php?id=18530, где 18530 и есть искомый параметр.
+# Проверка конфига
 if [ -f ~/.config/boorulogins.conf ]
 then
   . ~/.config/boorulogins.conf
@@ -41,6 +42,7 @@ else
   echo pixpass=ВАШ ПАРОЛЬ
   exit 5
 fi
+
 
 # поиск и удаление дублей
 finddups () {
@@ -64,24 +66,26 @@ finddups () {
 
 } # finddups
 
-# логинимся (куки в pixiv.txt)
-pixlogin () {
-# ярлык на страницу автора для общей кучи
-echo \[InternetShortcut\] > "$savedir.url"
-echo URL=http\:\/\/www.pixiv.net\/member_illust.php\?id=$athid >> "$savedir.url"
-echo Logging in...
-AUTH=`curl -k -s -c pixiv.txt --data "username=${pixid}&password=${pixpass}&grant_type=password&client_id=bYGKuGVw91e0NMfPGp44euvGt59s&client_secret=HP3RmkgAmEGro0gn1x9ioawQE8WMfvLXDz3ZqxpK" https://oauth.secure.pixiv.net/auth/token -A "$uag"`
 
-# Проверка логина
-checklog=`cat pixiv.txt |grep PHPSESSID|wc -l`
-if [ $checklog -eq 0 ]
-then
-  echo ERROR: Проверьте логин и пароль
-  rm pixiv.txt
-  exit 2
-else
-  echo OK
-fi
+# логинимся (куки в pixiv.txt, access_token в AUTH)
+# client_id и client_secret от приложения для iphone
+pixlogin () {
+  # ярлык на страницу автора для общей кучи
+  echo \[InternetShortcut\] > "$savedir.url"
+  echo URL=http\:\/\/www.pixiv.net\/member_illust.php\?id=$athid >> "$savedir.url"
+  echo Logging in...
+  AUTH=`curl -k -s -c pixiv.txt --data "username=${pixid}&password=${pixpass}&grant_type=password&client_id=bYGKuGVw91e0NMfPGp44euvGt59s&client_secret=HP3RmkgAmEGro0gn1x9ioawQE8WMfvLXDz3ZqxpK" https://oauth.secure.pixiv.net/auth/token -A "$uag"|pcregrep -o -e 'access_token\":\"[^\"]+'|sed 's#access_token":"##g'`
+
+  # Проверка логина
+  checklog=`cat pixiv.txt |grep PHPSESSID|wc -l`
+  if [ $checklog -eq 0 ]
+  then
+    echo ERROR: Проверьте логин и пароль
+    rm pixiv.txt
+    exit 2
+  else
+    echo OK
+  fi
 }
 
 # функция для получения списков
@@ -91,209 +95,60 @@ getlist () {
 picnum=1
 pagenum=1
 
+# пустые файлы
+touch get.pixiv.illist.txt get.pixiv.anim.txt
+
+# перебор страниц
 until [ $picnum -eq 0 ]
 do
-  # страница для парсинга
+  # страница для парсинга во временный файл
   echo Page $pagenum
-  curl -# -b pixiv.txt "http://www.pixiv.net/member_illust.php?type=$1&id=$athid&p=$pagenum" -A "$uag" --referer "http://www.pixiv.net/" > out.dat
-  
-  # Самый старый формат (скорее всего уже ничего не даст, но пусть будет)
-  cat out.dat|pcregrep -o -e 'http\:\/\/i\d{1,3}\.pixiv\.net\/img\d{1,3}\/img\/[^\"]+' > out.int.txt
-  # Вторая редакция (основная масса)
-  cat out.dat|pcregrep -o -e 'http\:\/\/i\d{1,3}\.pixiv\.net\/img-inf\/img\/[^\"]+' >> out.int.txt
-  # Третья редакция (26.09.2014). Заканчивается на _p0
-  cat out.dat|pcregrep -o -e "http\:\/\/i\d{1,3}\.pixiv\.net\/c\/150x150\/img-master[^\"]+" > out.new.txt
-  # Все вместе
-  cat out.int.txt|sed 's/_s\./\./' | sed 's/\?.*//' > out.txt
+  curl -# "https://public-api.secure.pixiv.net/v1/users/$athid/works.json?image_sizes=large&page=$pagenum&per_page=100" -H "Authorization: Bearer $AUTH"|pcregrep --buffer-size 1M -o -e 'large\"\:\"[^\"]+'|sed 's#large":"##g'|sort|uniq > out.tmp.txt
   # Сколько нашли на текущей странице?
-  picnum=`cat out.txt out.new.txt|wc -l`
+  picnum=`cat out.tmp.txt|wc -l`
   if [ $picnum \> 0 ]
   then
-    cat out.txt >> get.pixiv.$2.txt
-    if [ -s out.new.txt ]
+    # парсим
+    # иллюстрации и альбомы
+    illistcount=`cat out.tmp.txt|grep -v ugoira|wc -l`
+    if [ $illistcount \> 0 ]
     then
-      cat out.new.txt >> out.new.$2.txt
+      basename -a `cat out.tmp.txt|grep -v ugoira`|sed 's#_.*##g' >> get.pixiv.illist.txt
+    fi
+    # анимация
+    illistcount=`cat out.tmp.txt|grep ugoira|wc -l`
+    if [ $illistcount \> 0 ]
+    then
+      basename -a `cat out.tmp.txt|grep ugoira`|sed 's#_.*##g' >> get.pixiv.anim.txt
     fi
     let "pagenum++"
   fi
 done;
 
-# Если вдруг вообще ничего не нашли
-touch get.pixiv.$2.txt get.pixiv.$2.alt.txt get.pixiv.$2.new.txt
-
-if [ -s get.pixiv.$2.txt ]
-then
-  # Отделяем вторую редакцию от всего списка
-  basename -a `cat get.pixiv.$2.txt| grep img-inf`|sed 's/\..*//'| sed 's/-.*//g' > get.pixiv.$2.alt.txt
-fi
-
-if [ -s out.new.$2.txt ]
-then
-  # Третья редакция. basename может ругнуться
-  basename -a `cat out.new.$2.txt`|sed 's/\..*//'| sed 's/_p0_master1200//g'| sed 's/-.*//g' > get.pixiv.$2.new.txt
-fi
-
 } # getlist
 
-# удаляем мусор
-rmtrash () {
-if [ ! $1 ]
-then
-  rm -f get*.txt *pixiv.txt list* out.*
-fi
-} # rmtrash
+########################
+# Илюстрации и альбомы #
+########################
 
-#########################
-# Одиночные изображения #
-#########################
+procillist () {
+  touch get.pixiv.dl.txt
 
-procsingle () {
-
-# Отделяем альбомы от одиночных изображений. Актуально для новых ссылок.
-# И костылик для альбомов, которые в категории "Манга", но на самом деле одиночные изображения
-
-for i in `cat get.pixiv.pics.new.txt get.pixiv.album.new.txt`
-do
-  ismanga=`curl -# -b pixiv.txt "http://www.pixiv.net/member_illust.php?mode=medium&illust_id=$i" --referer "http://www.pixiv.net/" -A "$uaf"|pcregrep --buffer-size=1M  -o -e 'mode=manga[^\"]+'|wc -l`
-  if [ $ismanga -gt 0 ]
-  then
-    echo $i >> get.pixiv.album.new.txt
-    echo [*] $i is album
-  else
-    echo $i >> get.pixiv.pics.alt.txt
-    echo [*] $i is single image
-  fi
-done;
-
-# Сортируем и вычленяем настоящие альбомы.
-cat get.pixiv.album.new.txt|sort|uniq > get.pixiv.album.new.sort.txt
-cat get.pixiv.pics.alt.txt|sort > get.pixiv.pics.alt.sort.txt
-comm -2 -3 get.pixiv.album.new.sort.txt get.pixiv.pics.alt.sort.txt > get.pixiv.album.new.txt
-
-# Обрабатываем отфильтрованное
-for i in `cat get.pixiv.pics.alt.txt`
-do
-  curl -# "http://www.pixiv.net/member_illust.php?mode=big&illust_id=$i" -b pixiv.txt --referer "http://www.pixiv.net/member_illust.php?mode=medium&illust_id=$i" -A "$uag"|pcregrep --buffer-size=1M -o -e 'http\:\/\/i\d{1,3}\.pixiv\.net\/img[^\"]+' >> get.pixiv.pics.dl.txt
-done;
-
-# Скачивание
-if [ -s get.pixiv.pics.dl.txt ] 
-then
-  cat get.pixiv.pics.dl.txt| sed 's/\?.*//'  > get.pixiv.pics.clean.txt
-  $dldr -i get.pixiv.pics.clean.txt --referer="http://www.pixiv.net/"
-fi
-
-} # procsingle
-
-###########################
-# Альбомы с изображениями #
-###########################
-
-#############################
-# Обработка старых альбомов #
-#############################
-
-procoldalbums () {
-
-if [ -s get.pixiv.album.alt.txt ]
-then
-  for i in `cat get.pixiv.album.alt.txt`
+  # Обрабатываем все найденные ID
+  for i in `cat get.pixiv.illist.txt`
   do
-    curl -# "http://www.pixiv.net/member_illust.php?mode=manga&illust_id=$i&type=scroll" -b pixiv.txt --referer "http://www.pixiv.net/" -A "$uag"|pcregrep --buffer-size=1M -o -e "http\:\/\/i\d{1,3}\.pixiv\.net\/img\d{1,3}\/img\/[^(\'|\?|\")]+" -e "http\:\/\/i\d{1,3}\.pixiv\.net\/img-inf\/img\/[^(\'|\?|\")]+"| sed -e 's/\?.*//'>> get.pixiv.album.dl.alt.txt
+    echo Processing $i...
+    curl -# "https://public-api.secure.pixiv.net/v1/works/$i.json?image_sizes=large" -H "Authorization: Bearer $AUTH"|pcregrep --buffer-size 1M -o -e 'large\"\:\"[^\"]+'|sed 's#large":"##g'|sort|uniq >> get.pixiv.dl.txt
   done;
-fi
 
-# Чистка от мусора и левых ссылок
-if [ -s get.pixiv.album.dl.alt.txt ]
-then
-  cat get.pixiv.album.dl.alt.txt|grep -v '\/mobile\/'|sort|uniq > get.pixiv.album.dl.clean.txt
-  mv get.pixiv.album.dl.clean.txt get.pixiv.album.dl.alt.txt
-  # Костыль для обхода  _p в именах юзеров
-  dirname `cat get.pixiv.album.dl.alt.txt` > get.pixiv.album.dl.alt.dir.txt
-  basename -a `cat get.pixiv.album.dl.alt.txt`|sed -e 's/_p/_big_p/g' > get.pixiv.album.dl.alt.fn.txt
-  paste -d "/" get.pixiv.album.dl.alt.dir.txt get.pixiv.album.dl.alt.fn.txt|sort > get.pixiv.album.dl.alt.txt
-  # Закачка
-  $dldr -i get.pixiv.album.dl.alt.txt --referer="http://www.pixiv.net/"
-fi
-
-# Список скаченного
-ls *.jpg *.png *.gif|grep big|sed 's/_big[^\.]*//g'|sed 's/\..*//g'|sort|uniq > get.pixiv.album.dld.txt
-
-# get.pixiv.album.dl.alt.txt   - список id всех альбомов
-# get.pixiv.album.dld.txt      - список скаченного
-# get.pixiv.album.small.txt    - список нескаченного
-
-cat get.pixiv.album.alt.txt|sort > get.pixiv.album.sort.alt.txt
-comm -2 -3 get.pixiv.album.sort.alt.txt get.pixiv.album.dld.txt|sort|uniq -u > get.pixiv.album.small.txt
-
-if [ -s get.pixiv.album.small.txt ]
-then
-  for i in `cat get.pixiv.album.small.txt`
-  do
-    curl -# "http://www.pixiv.net/member_illust.php?mode=manga&illust_id=$i&type=scroll" -b pixiv.txt --referer "http://www.pixiv.net/" -A "$uag" > out.dat
-    cat out.dat|pcregrep --buffer-size=1M -o -e "http\:\/\/i\d{1,3}\.pixiv\.net\/img\d{1,3}\/img\/[^(\'|\?|\")]+" -e "http\:\/\/i\d{1,3}\.pixiv\.net\/img-inf\/img\/[^(\'|\?|\")]+" >> get.pixiv.album.dl.small.txt
-    # для обработки скриптового листания манги
-    cat out.dat|sed 's#\\\/#\/#g'|pcregrep --buffer-size=1M -o -e "http\:\/\/i\d{1,3}\.pixiv\.net\/img\d{1,3}\/img\/[^(\'|\?|\")]+" -e "http\:\/\/i\d{1,3}\.pixiv\.net\/img[^\")]+"|grep -v '/mobile/'|grep 'big' >> get.pixiv.album.dl.small.txt
-  done;
-  if [ -s get.pixiv.album.dl.small.txt ] 
+  # Скачивание
+  if [ -s get.pixiv.dl.txt ] 
   then
-    cat get.pixiv.album.dl.small.txt|grep -v '\/mobile\/'|sort|uniq > get.pixiv.album.dl.small.clean.txt
-    mv get.pixiv.album.dl.small.clean.txt get.pixiv.album.dl.small.txt
-    $dldr -i get.pixiv.album.dl.small.txt --referer="http://www.pixiv.net/"
+    $dldr -i get.pixiv.dl.txt --referer="http://www.pixiv.net/"
   fi
-fi
 
-} # procoldalbums
+} # procillist
 
-############################
-# Обработка новых альбомов #
-############################
-
-procnewalbums () {
-
-if [ -s get.pixiv.album.new.txt ]
-then
-  for i in `cat get.pixiv.album.new.txt`
-  do
-    curl -# "http://www.pixiv.net/member_illust.php?mode=manga&illust_id=$i&type=scroll" -b pixiv.txt --referer "http://www.pixiv.net/" -A "$uag"|pcregrep --buffer-size=1M -o -e "http\:\/\/i\d{1,3}\.pixiv\.net\/c[^\"]+"| sed -e 's#c\/1200x1200\/img-master#img-original#g' -e 's/_master1200//'>> get.pixiv.album.dl.new.txt
-  done;
-  if [ -s get.pixiv.album.dl.new.txt ] 
-  then
-    $dldr -i get.pixiv.album.dl.new.txt --referer="http://www.pixiv.net/"
-  fi
-fi
-
-# URL больших картинок могут не совпадать с маленькими
-# Отдельный парсер для таких случаев
-if [ -s get.pixiv.album.dl.new.txt ]
-then
-  basename -a `cat get.pixiv.album.dl.new.txt`|sed -e 's#_.*##g' -e 's/-.*//g' |uniq > get.pixiv.albums.bad.txt
-  for i in `cat get.pixiv.albums.bad.txt`
-  do
-    # Если файлов меньше одного, то альбом не скачался
-    if [ `ls $i*|grep -v _big |wc -l` -lt `cat get.pixiv.album.dl.new.txt|grep $i|wc -l` ]
-      then
-        pagenum=0
-        picnum=1
-        # пока ни одной сслыки не будет найдено (страница с ошибкой)
-        until [ $picnum -eq 0 ]
-        do
-          curl -# "http://www.pixiv.net/member_illust.php?mode=manga_big&illust_id=$i&page=$pagenum"  -b pixiv.txt --referer "http://www.pixiv.net/member_illust.php?mode=manga&illust_id=$i" -A "$uag" > out.dat
-          picnum=`cat out.dat|pcregrep -o  -e 'http\:\/\/i\d{1,3}\.pixiv\.net\/img[^\"]+'|grep -v ugoira|wc -l`
-          cat out.dat|pcregrep -o  -e 'http\:\/\/i\d{1,3}\.pixiv\.net\/img[^\"]+' >> get.pixiv.albums.new.list.txt
-          let "pagenum++"
-        done;
-    else
-      echo [*] $i already downloaded.
-    fi
-  done;
-  if [ -s get.pixiv.albums.new.list.txt ] 
-  then
-    $dldr -i get.pixiv.albums.new.list.txt --referer="http://www.pixiv.net/"
-  fi
-fi
-
-} # procnewalbums
 
 ######################
 # Архивы с анимацией #
@@ -304,7 +159,7 @@ procanim () {
 # Здесь важны только id
 if [ -s get.pixiv.anim.txt ]
 then
-  for i in `cat get.pixiv.anim.alt.txt get.pixiv.anim.new.txt`
+  for i in `cat get.pixiv.anim.txt`
   do
     # Получение страницы
     curl -# "http://www.pixiv.net/member_illust.php?mode=medium&illust_id=$i" -b pixiv.txt --referer "http://www.pixiv.net/member_illust.php?mode=medium&illust_id=$i" -A "$uag" > out.ugo
@@ -323,6 +178,15 @@ fi
 
 } # procanim
 
+
+# удаляем мусор
+rmtrash () {
+if [ ! $1 ]
+then
+  rm -f get*.txt *pixiv.txt list* out.*
+fi
+} # rmtrash
+
 # очистка в любом случае
 trap rmtrash 1 2 3 15
 
@@ -339,19 +203,10 @@ flock -n 0
 if [ $? -eq 0 ]
 then
   pixlogin
-  echo [*] Building illust list...
-  getlist illust pics
-  echo [*] Building albums list...
-  getlist manga album
-  echo [*] Building animation list...
-  getlist ugoira anim
-  echo [*] Processing illust list...
-  procsingle
-  echo [*] Processing albums list...
-  echo [*] 1/2 old
-  procoldalbums
-  echo [*] 2/2 new
-  procnewalbums
+  echo [*] Building list...
+  getlist
+  echo [*] Processing illust and albums list...
+  procillist
   echo [*] Processing animation list...
   procanim
   echo [*] Removing dups...
