817b9437e32fc37b59f9b1f334dce988532dfd66
Pavel Frolov <tigrashik@gmail.com>
2017-03-29 19:05:44 +0300

    get.gelbooru.sh: http -> https

--- get.gelbooru.sh
+++ get.gelbooru.sh
@@ -31,7 +31,7 @@ echo Entering $savedir
 cd "$savedir"
 
 # Количество постов
-postcount=`curl -# "http://gelbooru.com/index.php?page=dapi&s=post&q=index&tags=$tags&limit=1" -A "$uag"|pcregrep -o 'posts\ count=\"[^"]+'|sed -e 's/posts\ count=//' -e 's/\"//'`
+postcount=`curl -# "https://gelbooru.com/index.php?page=dapi&s=post&q=index&tags=$tags&limit=1" -A "$uag"|pcregrep -o 'posts\ count=\"[^"]+'|sed -e 's/posts\ count=//' -e 's/\"//'`
 
 # Проверка количетсва
 if [ $postcount -eq 0 ]
@@ -53,7 +53,7 @@ pcount=`expr $postcount / 1000`
 for ((i=0; i<=$pcount; i++))
 do
   echo Page $i
-  curl -# "http://gelbooru.com/index.php?page=dapi&s=post&q=index&tags=$tags&limit=1000&pid=$i" -A "$uag"|pcregrep -o -e 'file_url=[^ ]+'|sed -e 's/file_url=/http\:/g' -e 's/\"//g' >>get2.gelbooru.txt
+  curl -# "https://gelbooru.com/index.php?page=dapi&s=post&q=index&tags=$tags&limit=1000&pid=$i" -A "$uag"|pcregrep -o -e 'file_url=[^ ]+'|sed -e 's/file_url=/https\:/g' -e 's/\"//g' >>get2.gelbooru.txt
 done;
 
-wget -nc -i get2.gelbooru.txt --referer="http://gelbooru.com/"
+wget -nc -i get2.gelbooru.txt --referer="https://gelbooru.com/" --no-check-certificate
