434e2be236c1bdfd6c971993cbd30b8aa9357013
Pavel Frolov <tigrashik@gmail.com>
2018-08-13 03:54:15 +0300

    UTF-8

--- get.sankaku.sh
+++ get.sankaku.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-# Юзергаент
+# Р®Р·РµСЂРіР°РµРЅС‚
 uag="Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:61.0) Gecko/20100101 Firefox/61.0"
 
-# Проверка параметров
+# РџСЂРѕРІРµСЂРєР° РїР°СЂР°РјРµС‚СЂРѕРІ
 if [ ! "$2" = "" ]
 then
   tags=$1
@@ -14,13 +14,13 @@ else
     tags=$1
     savedir=$1
   else
-    echo Использование:
-    echo `basename $0` теги \[каталог\]
+    echo РСЃРїРѕР»СЊР·РѕРІР°РЅРёРµ:
+    echo `basename $0` С‚РµРіРё \[РєР°С‚Р°Р»РѕРі\]
     exit 1
   fi
 fi
 
-# Каталог для закачки
+# РљР°С‚Р°Р»РѕРі РґР»СЏ Р·Р°РєР°С‡РєРё
 if [ ! -d $savedir ]
 then
   echo Creating $savedir
@@ -29,50 +29,50 @@ fi
 echo Entering $savedir
 cd "$savedir"
 
-# Получение логина и пароля
+# РџРѕР»СѓС‡РµРЅРёРµ Р»РѕРіРёРЅР° Рё РїР°СЂРѕР»СЏ
 
 if [ -f ~/.config/boorulogins.conf ]
 then
   . ~/.config/boorulogins.conf
 else
-  echo Файл с данными для авторизации не найден!
-  echo Создайте файл ~/.config/boorulogins.conf и поместите в него следующие строки:
-  echo sanlogin=ВАШ ЛОГИН
-  echo sanpass=ВАШ ПАРОЛЬ
+  echo Р¤Р°Р№Р» СЃ РґР°РЅРЅС‹РјРё РґР»СЏ Р°РІС‚РѕСЂРёР·Р°С†РёРё РЅРµ РЅР°Р№РґРµРЅ!
+  echo РЎРѕР·РґР°Р№С‚Рµ С„Р°Р№Р» ~/.config/boorulogins.conf Рё РїРѕРјРµСЃС‚РёС‚Рµ РІ РЅРµРіРѕ СЃР»РµРґСѓСЋС‰РёРµ СЃС‚СЂРѕРєРё:
+  echo sanlogin=Р’РђРЁ Р›РћР“РРќ
+  echo sanpass=Р’РђРЁ РџРђР РћР›Р¬
   exit 5
 fi
 
-# логинимся (куки в sankaku.txt)
-# Получение appkey
+# Р»РѕРіРёРЅРёРјСЃСЏ (РєСѓРєРё РІ sankaku.txt)
+# РџРѕР»СѓС‡РµРЅРёРµ appkey
 username=`echo $sanlogin| tr [:upper:] [:lower:]`
 appkey=`echo -n sankakuapp_${username}_Z5NE9YASej|sha1sum|cut -d" " -f1`
 echo Logging in...
 curl -s -c sankaku.txt -d "user[name]=${sanlogin}&user[password]=${sanpass}&appkey=${appkey}" https://capi-beta.sankakucomplex.com/user/authenticate.json > sanlogin.txt
 
-# Проверка логина
+# РџСЂРѕРІРµСЂРєР° Р»РѕРіРёРЅР°
 checklog=`cat sanlogin.txt |grep 'success":false'|wc -l`
 if [ $checklog -ge 1 ]
 then
-  echo ERROR: Проверьте логин и пароль
+  echo ERROR: РџСЂРѕРІРµСЂСЊС‚Рµ Р»РѕРіРёРЅ Рё РїР°СЂРѕР»СЊ
   rm sanlogin.txt
 #  exit 2
 else
   echo OK
 fi
 
-# Удаление старого списка
+# РЈРґР°Р»РµРЅРёРµ СЃС‚Р°СЂРѕРіРѕ СЃРїРёСЃРєР°
 if [ -e get.sankaku.txt ]
 then
   rm -f get.sankaku.txt
 fi
 
-# Загрузка до тех пор, пока в выдаче будет 0 ссылок
+# Р—Р°РіСЂСѓР·РєР° РґРѕ С‚РµС… РїРѕСЂ, РїРѕРєР° РІ РІС‹РґР°С‡Рµ Р±СѓРґРµС‚ 0 СЃСЃС‹Р»РѕРє
 pagenum=1
 picnum=1
 
 until [ $picnum -eq 0 ]
 do
-  # Получение списка
+  # РџРѕР»СѓС‡РµРЅРёРµ СЃРїРёСЃРєР°
   echo Page $pagenum
   curl -# -b sankaku.txt "https://capi-beta.sankakucomplex.com/post/index.json?tags=$tags&page=$pagenum&limit=100" --referer "https://chan.sankakucomplex.com/" -A "$uag" | pcregrep --buffer-size=1M -o -e 'file_url\":\"[^\"]+'|sed -e 's#\\u0026#\&#g' -e 's#file_url":"#https:#g' > tmp.sankaku.txt
   picnum=`cat tmp.sankaku.txt|wc -l`
@@ -83,20 +83,20 @@ do
   fi
 done;
 
-# Проверка количетсва
+# РџСЂРѕРІРµСЂРєР° РєРѕР»РёС‡РµС‚СЃРІР°
 postcount=`cat get.sankaku.txt|wc -l`
 
 if [ $postcount -eq 0 ]
 then
-  echo По сочетанию "$tags" ничего не найдено.
+  echo РџРѕ СЃРѕС‡РµС‚Р°РЅРёСЋ "$tags" РЅРёС‡РµРіРѕ РЅРµ РЅР°Р№РґРµРЅРѕ.
   rm -f tmp.sankaku.txt sankaku.txt
   exit 3
 else
-  echo По сочетанию "$tags" найдено постов: $postcount
+  echo РџРѕ СЃРѕС‡РµС‚Р°РЅРёСЋ "$tags" РЅР°Р№РґРµРЅРѕ РїРѕСЃС‚РѕРІ: $postcount
 fi
 
 #wget --random-wait --no-check-certificate -nc -i get.sankaku.txt -U "$uag"
 aria2c --allow-overwrite=true --auto-file-renaming=false --conditional-get=true --remote-time -x10 -s10 -i get.sankaku.txt
 
-# убираем за собой
+# СѓР±РёСЂР°РµРј Р·Р° СЃРѕР±РѕР№
 rm -f tmp.sankaku.txt sankaku.txt sanlogin.txt
